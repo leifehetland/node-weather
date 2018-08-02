a3c46ca119e671bbce65b2414e15165bb853b452
Leif Hetland <lehetland1@gmail.com>
2018-08-02 16:29:04 -0400

    basic error handling

--- app.js
+++ app.js
@@ -14,20 +14,20 @@ const argv = yargs
 	.alias('help', 'h')
 	.argv;
 
-	console.log('test: ',argv);
-
-	let encodedAddress = encodeURIComponent(argv.address);
-
-	console.log(encodedAddress);
-	
-	
+let encodedAddress = encodeURIComponent(argv.address);
 
 request({
 	url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
 	json: true
 }, (error, response, body) => {
-	// console.log(JSON.stringify(body, undefined, 2));
-	console.log(`Address: ${body.results[0].formatted_address}`);
-	console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
-	console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+	if (error) {
+		console.log('Unable to connect to Google Servers.');
+	} else if (body.status === 'ZERO_RESULTS') {
+		console.log('Unable to find that address.');
+	} else if (body.status === 'OK') {
+		// console.log(JSON.stringify(body, undefined, 2));
+		console.log(`Address: ${body.results[0].formatted_address}`);
+		console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+		console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+	}
 });
