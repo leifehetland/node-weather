067f4b829e736652f36457b362f5bf3f0b2e726a
Leif Hetland <lehetland1@gmail.com>
2018-08-02 16:10:59 -0400

    encoding user input using yargs

--- app.js
+++ app.js
@@ -1,8 +1,33 @@
 const request = require('request');
+const yargs = require('yargs');
+
+const argv = yargs
+	.options({
+		a: {
+			demand: true,
+			alias: 'address',
+			describe: 'Address to fetch weather for',
+			string: true
+		}
+	})
+	.help()
+	.alias('help', 'h')
+	.argv;
+
+	console.log('test: ',argv);
+
+	let encodedAddress = encodeURIComponent(argv.address);
+
+	console.log(encodedAddress);
+	
+	
 
 request({
-	url: 'https://maps.googleapis.com/maps/api/geocode/json?address=%20stavanger%20norway',
+	url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
 	json: true
 }, (error, response, body) => {
-	console.log(JSON.stringify(body, undefined, 2));
+	// console.log(JSON.stringify(body, undefined, 2));
+	console.log(`Address: ${body.results[0].formatted_address}`);
+	console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+	console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
 });
